15ee2a4485f5593c0c574888379fba1aac748c9d
jrcidade@gmail.com <Print("github")>
2022-08-05 15:09:31 -0400

    Config.004

--- core/settings.py
+++ core/settings.py
@@ -120,9 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Manaus'
 
 USE_I18N = True
 
@@ -143,6 +143,9 @@ STATICFILES_DIRS = (
     os.path.join(CORE_DIR, 'apps/static'),
 )
 
+
+# Close the session quitting the browser
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 #############################################################
 #############################################################
